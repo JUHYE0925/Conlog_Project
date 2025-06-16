a56be645614981e25802633d46bd74e930d90e30
DESKTOP-43RUNA1\USER <rhkdlf48301@gmail.com>
2025-06-17 01:27:52 +0900

    [FEATURE] #3/Review/selectOneReview 내가 작성한 리뷰 중 리뷰 번호를 통해 상세조회

--- BACK-END/conlog-backend/src/main/java/com/conlog/config/SwaggerConfig.java
+++ BACK-END/conlog-backend/src/main/java/com/conlog/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.conlog.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -10,6 +11,15 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @EnableWebMvc
 public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicAPI(){
+
+        return GroupedOpenApi.builder()
+                .group("conlog-public")
+                .pathsToMatch("/**")
+                .build();
+    }
 
     @Bean
     public OpenAPI openAPI(){
